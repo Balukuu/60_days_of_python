12b3f3dab429ab1498d624b94cc9817bb1f5a20c
Edgar Baluku <edgarbaluku@gmail.com>
2023-10-30 21:53:07 +0300

    for loop

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+#For Loops
+#For loops are used for iterating over a sequence (e.g., a list, tuple, string, or range) or other iterable objects.
+
+fruits = ["mango","banana","apples"]
+for fruit in fruits:
+    print(fruits)
