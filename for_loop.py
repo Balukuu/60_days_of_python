6344681d2cc3d80e585d0006c76c85ea1f7ee361
Edgar Baluku <edgarbaluku@gmail.com>
2023-10-30 22:02:15 +0300

    for loop

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,4 @@
 #For Loops
 #For loops are used for iterating over a sequence (e.g., a list, tuple, string, or range) or other iterable objects.
 
-for x in range(4):
-    print ("Baluku")
+print ("mweow\n" * 3, end ="")
