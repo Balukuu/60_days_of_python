285b2f6b2a26b732eafcc00e636860ed4958b828
Edgar Baluku <edgarbaluku@gmail.com>
2023-10-30 21:45:08 +0300

    while loop

--- cat.py
+++ cat.py
@@ -1,3 +1,8 @@
+#While Loops:
+#While loops are used when you want to execute a block of code 
+#as long as a certain condition is true. 
+#The code block continues to execute until the condition becomes false.
+
 i = 0
 while i < 3 :
     print("meow")
